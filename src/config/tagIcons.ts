a3e367131dba4a88292abb2c335b4f6a606fc68d
yoontako <129288760+yoontako@users.noreply.github.com>
2025-03-27 21:12:00 +0900

    add

--- src/config/tagIcons.ts
+++ src/config/tagIcons.ts
@@ -1,6 +1,4 @@
 export const tagIcons: { [key: string]: string } = {
   'Scrapbox': '/scrapbox.png',
-  'JavaScript': '/icons/javascript.svg',
-  'TypeScript': '/icons/typescript.svg',
-  // 他のタグとアイコンのマッピングを追加
+  'Mac': '/apple.png',
 }; 
